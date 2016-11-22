d8a1ca9d1e6b7e6a66bc8bb6f0c0420143f1f13c
Dylan <dylanninin@gmail.com>
2016-11-22 10:57:59 +0800

    Fix typo

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
         // node with the id "app", so that the client-side
         // version can take over once it loads
         return new Vue({
-            template: <'div id="app"> You have been here for {{ counter }} seconds.</div>',
+            template: '<div id="app"> You have been here for {{ counter }} seconds.</div>',
             data: {
                 counter: 0
             },
